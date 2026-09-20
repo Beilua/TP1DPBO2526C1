aaba0aa10f81effc605591f838f732fd51209fe9
Nabila Attaya Putri Cahyadi <axoliolio@gmail.com>
2026-09-20 11:11:40 +0700

    fix: error handling logic for ticket price and rating in add and update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -67,17 +67,23 @@ int main() {
 
         if (command == "/add") {
             addFilm(filmList, genres);
-        } else if (command == "/display") {
+        } 
+        else if (command == "/display") {
             displayFilms(filmList);
-        } else if (command == "/update") {
+        } 
+        else if (command == "/update") {
             updateFilm(filmList, genres);
-        } else if (command == "/delete") {
+        } 
+        else if (command == "/delete") {
             deleteFilm(filmList);
-        } else if (command == "/search") {
+        } 
+        else if (command == "/search") {
             searchFilm(filmList);
-        } else if (command == "/martin") {
+        } 
+        else if (command == "/martin") {
             absoluteCinema();
-        } else if (command == "/exit") {
+        } 
+        else if (command == "/exit") {
             cout << "                          _  _                      " << '\n';
             cout << "                         ( )( )                     " << '\n';
             cout << "   __     _      _      _| || |_    _   _    __     " << '\n';
@@ -87,7 +93,8 @@ int main() {
             cout << "( )_) |                            ( )_| |          " << '\n';
             cout << " \\___/'                            `\\___/'          " << '\n';
             exit(0);
-        } else {
+        } 
+        else {
             cout << "\ninvalid command.\n";
         }
     }
@@ -156,19 +163,24 @@ void addFilm(vector<CinemaFilm>& filmList, const vector<string>& genres) {
             durationInput = durationInput.substr(0, durationInput.find_last_not_of(" \t\r\n") + 1);
             if (regex_match(durationInput, regex("^-?\\d+\\.\\d+$"))) {
                 cout << "non absolute cinema. invalid duration. no decimal numbers allowed.\n";
-            } else {
+            } 
+            else {
                 try {
                     duration = stoi(durationInput);
                     if (duration < 0) {
                         cout << "non absolute cinema. invalid duration. negative numbers are not allowed.";
-                    } else if (duration > 873) {
+                    } 
+                    else if (duration > 873) {
                         cout << "non absolute cinema. even the longest cinema film in history is only 873 minutes long. please enter a valid duration.";
-                    } else {
+                    } 
+                    else {
                         break;
                     }
-                } catch (const invalid_argument&) {
+                } 
+                catch (const invalid_argument&) {
                     cout << "non absolute cinema. invalid duration. input is not a valid number.";
-                } catch (const out_of_range&) {
+                } 
+                catch (const out_of_range&) {
                     cout << "non absolute cinema. invalid duration. input is not a valid number.";
                 }
                 cout << "\n";
@@ -179,15 +191,71 @@ void addFilm(vector<CinemaFilm>& filmList, const vector<string>& genres) {
 
         cout << "ticket price (us dollars): ";
         int ticketPrice;
-        string ticketPriceInput;
-        getline(cin, ticketPriceInput);
-        ticketPrice = stoi(ticketPriceInput);
+        while (true) {
+            string ticketPriceInput;
+            getline(cin, ticketPriceInput);
+            ticketPriceInput = ticketPriceInput.substr(0, ticketPriceInput.find_last_not_of(" \t\r\n") + 1);
+            if (regex_match(ticketPriceInput, regex("^-?\\d+\\.\\d+$"))) {
+                cout << "non absolute cinema. invalid ticket price. no decimal numbers allowed.\n";
+            } 
+            else {
+                try {
+                    ticketPrice = stoi(ticketPriceInput);
+                    if (ticketPrice < 0) {
+                        cout << "non absolute cinema. invalid ticket price. negative numbers are not allowed.";
+                    } 
+                    else if (ticketPrice > 500) {
+                        cout << "non absolute cinema. invalid ticket price. the maximum ticket price is 500 dollars.";
+                    } 
+                    else {
+                        break;
+                    }
+                } 
+                catch (const invalid_argument&) {
+                    cout << "non absolute cinema. invalid ticket price. input is not a valid number.";
+                } 
+                catch (const out_of_range&) {
+                    cout << "non absolute cinema. invalid ticket price. input is not a valid number.";
+                }
+                cout << "\n";
+            }
+            cout << "\n";
+            cout << "ticket price (us dollars): ";
+        }
 
         cout << "rating (out of 10): ";
         int rating;
-        string ratingInput;
-        getline(cin, ratingInput);
-        rating = stoi(ratingInput);
+        while (true) {
+            string ratingInput;
+            getline(cin, ratingInput);
+            ratingInput = ratingInput.substr(0, ratingInput.find_last_not_of(" \t\r\n") + 1);
+            if (regex_match(ratingInput, regex("^-?\\d+\\.\\d+$"))) {
+                cout << "non absolute cinema. invalid rating. no decimal numbers allowed.\n";
+            } 
+            else {
+                try {
+                    rating = stoi(ratingInput);
+                    if (rating < 0) {
+                        cout << "non absolute cinema. invalid rating. negative numbers are not allowed.";
+                    } 
+                    else if (rating > 10) {
+                        cout << "non absolute cinema. invalid rating. the maximum rating is 10.";
+                    } 
+                    else {
+                        break;
+                    }
+                } 
+                catch (const invalid_argument&) {
+                    cout << "non absolute cinema. invalid rating. input is not a valid number.";
+                } 
+                catch (const out_of_range&) {
+                    cout << "non absolute cinema. invalid rating. input is not a valid number.";
+                }
+                cout << "\n";
+            }
+            cout << "\n";
+            cout << "rating (out of 10): ";
+        }
 
         CinemaFilm newFilm(code, title, genre, duration, ticketPrice, rating);
         filmList.push_back(newFilm);
@@ -241,19 +309,24 @@ void updateFilm(vector<CinemaFilm>& filmList, const vector<string>& genres) {
                     durationInput = durationInput.substr(0, durationInput.find_last_not_of(" \t\r\n") + 1);
                     if (regex_match(durationInput, regex("^-?\\d+\\.\\d+$"))) {
                         cout << "non absolute cinema. invalid duration. no decimal numbers allowed.\n";
-                    } else {
+                    } 
+                    else {
                         try {
                             newDuration = stoi(durationInput);
                             if (newDuration < 0) {
                                 cout << "non absolute cinema. invalid duration. negative numbers are not allowed.";
-                            } else if (newDuration > 873) {
+                            } 
+                            else if (newDuration > 873) {
                                 cout << "non absolute cinema. even the longest cinema film in history is only 873 minutes long. please enter a valid duration.";
-                            } else {
+                            } 
+                            else {
                                 break;
                             }
-                        } catch (const invalid_argument&) {
+                        } 
+                        catch (const invalid_argument&) {
                             cout << "non absolute cinema. invalid duration. input is not a valid number.";
-                        } catch (const out_of_range&) {
+                        } 
+                        catch (const out_of_range&) {
                             cout << "non absolute cinema. invalid duration. input is not a valid number.";
                         }
                         cout << "\n";
@@ -264,14 +337,74 @@ void updateFilm(vector<CinemaFilm>& filmList, const vector<string>& genres) {
                 film.setFilmDuration(newDuration);
 
                 cout << "new ticket price (us dollars): ";
-                string newTicketPriceInput;
-                getline(cin, newTicketPriceInput);
-                film.setTicketPrice(stoi(newTicketPriceInput));
+                int newTicketPrice;
+                while (true) {
+                    string newTicketPriceInput;
+                    getline(cin, newTicketPriceInput);
+                    newTicketPriceInput = newTicketPriceInput.substr(0, newTicketPriceInput.find_last_not_of(" \t\r\n") + 1);
+                    if (regex_match(newTicketPriceInput, regex("^-?\\d+\\.\\d+$"))) {
+                        cout << "non absolute cinema. invalid ticket price. no decimal numbers allowed.\n";
+                    } 
+                    else {
+                        try {
+                            newTicketPrice = stoi(newTicketPriceInput);
+                            if (newTicketPrice < 0) {
+                                cout << "non absolute cinema. invalid ticket price. negative numbers are not allowed.";
+                            } 
+                            else if (newTicketPrice > 500) {
+                                cout << "non absolute cinema. invalid ticket price. the maximum ticket price is 500 dollars.";
+                            } 
+                            else {
+                                break;
+                            }
+                        } 
+                        catch (const invalid_argument&) {
+                            cout << "non absolute cinema. invalid ticket price. input is not a valid number.";
+                        } 
+                        catch (const out_of_range&) {
+                            cout << "non absolute cinema. invalid ticket price. input is not a valid number.";
+                        }
+                        cout << "\n";
+                    }
+                    cout << "\n";
+                    cout << "new ticket price (us dollars): ";
+                }
+                film.setTicketPrice(newTicketPrice);
 
                 cout << "new rating (out of 10): ";
-                string newRatingInput;
-                getline(cin, newRatingInput);
-                film.setFilmRating(stoi(newRatingInput));
+                int newRating;
+                while (true) {
+                    string newRatingInput;
+                    getline(cin, newRatingInput);
+                    newRatingInput = newRatingInput.substr(0, newRatingInput.find_last_not_of(" \t\r\n") + 1);
+                    if (regex_match(newRatingInput, regex("^-?\\d+\\.\\d+$"))) {
+                        cout << "non absolute cinema. invalid rating. no decimal numbers allowed.\n";
+                    } 
+                    else {
+                        try {
+                            newRating = stoi(newRatingInput);
+                            if (newRating < 0) {
+                                cout << "non absolute cinema. invalid rating. negative numbers are not allowed.";
+                            } 
+                            else if (newRating > 10) {
+                                cout << "non absolute cinema. invalid rating. the maximum rating is 10.";
+                            } 
+                            else {
+                                break;
+                            }
+                        } 
+                        catch (const invalid_argument&) {
+                            cout << "non absolute cinema. invalid rating. input is not a valid number.";
+                        } 
+                        catch (const out_of_range&) {
+                            cout << "non absolute cinema. invalid rating. input is not a valid number.";
+                        }
+                        cout << "\n";
+                    }
+                    cout << "\n";
+                    cout << "new rating (out of 10): ";
+                }
+                film.setFilmRating(newRating);
 
                 cout << "absolute cinema. film has been updated.\n\n";
                 return;
